b5f751755a227bc8ffef79f6991ad536246787af
Artermis <nopphadon.phanwong@gmail.com>
2022-01-23 15:10:45 +0700

    Finish Maskify

--- Code/src/main/java/Maskify.java
+++ Code/src/main/java/Maskify.java
@@ -1,6 +1,20 @@
 public class Maskify {
     public static String maskify(String str) {
-        throw new UnsupportedOperationException("Unimplemented");
+        String ans="";
+        if(str.length() <= 4){
+            return str;
+        }
+        else{
+            String last_4 = str.substring(str.length() - 4);
+            int len_string = str.length() - 4;
+            for (int i = 0; i < len_string; i++) {
+                System.out.println(i);
+            }
+            return ans + last_4;
+        }
+
+
+
     }
     public String name(){
         return "Golf";
